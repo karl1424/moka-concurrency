52918e43226010f302e3a151726ac7cd432dd6a3
Weihao Mo <s235284@dtu.dk>
2026-03-27 09:29:31 +0100

    chore: added more examples in generator

--- apps/chip/src/lib/components/Generator.svelte
+++ apps/chip/src/lib/components/Generator.svelte
@@ -12,17 +12,83 @@
 
   const examples: Example[] = [
     {
-      title: 'Tuple space operation - getP',
-      code: `> ts = (Q, INF, {(1,1), (1,2),(1,3)})
-do ts.getP(1, ?x) -> z := z + 1
-od
+      title: 'Array assignment',
+      code: `> A = [1,2,3]
+A[0] := 2;
+A[1] := 3
+
+check A[0] = 1 & A[1] = 2
+check X (A[0] = 2 & A[1] = 2)
+check X X (A[0] = 2 & A[1] = 3)
+check G A[2] = 3
 check F terminated`,
     },
     {
-      title: 'Channel operation - send and receive',
-      code: `> c = (INF, ())
-c!1;
-c?x`,
+      title: `Tuple space operations`,
+      code: `> ts = (R, INF, {(3,4),(5,6)})
+ts.put(1,x);
+ts.get(1,?a);
+ts.query(?b,4)
+
+check G ts.putP(1)
+check G ts.getP(3,4)
+check G ts.getP(5,6)
+check G !ts.getP(1)
+check G ts.queryP(3,4)
+check G ts.queryP(5,6)
+check F a = 0
+check F b = 3
+check F terminated`,
+    },
+    {
+      title: 'Asynchronous Communication - send and receive',
+      code: `> c = (1, (5))
+c?x;
+c!10
+check F (x = 5 & c??10)
+check X X (c??10)
+check F (c?10)
+check F terminated`,
+    },
+    {
+      title: 'Synchronous Communication - send and receive',
+      code: `> c = (0)
+par
+c!1
+[]
+c?x
+rap
+
+check F terminated
+check F x = 1`,
+    },
+    {
+      title: 'Broadcast',
+      code: `> c = (0), x = 0
+par
+c!!2 2
+[]
+c?x
+[]
+c?y
+rap
+
+check F (x = 2 & y = 2)
+check F terminated`,
+    },
+    {
+      title: 'Gather',
+      code: `> c = (0), A = [0,0], x = 0
+par
+c??2 A x
+[]
+c!1
+[]
+c!2
+rap
+
+check F (x = 2 & A[0] = 1 & A[1] = 2) | F (x = 2 & A[0] = 2 & A[1] = 1)
+check F terminated`,
     },
   ];
 </script>
